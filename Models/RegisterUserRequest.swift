220aaf6179e12289ab982bda90f31eaf45edd4b6
Zhan_Dark <126955817+ZhannaDark@users.noreply.github.com>
2024-01-31 14:59:19 +0600

    Create RegisterUserRequest.swift

---
+++ Models/RegisterUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterUserRequest.swift
+//  FinalProject
+//
+//  Created by Zhanna Zhanashova on 21.12.2023.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let username: String
+    let email: String
+    let password: String
+}
